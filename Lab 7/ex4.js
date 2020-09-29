2a4ad5913b45d26acb533f087cd632e7f8eac757
reiyama622 <--git>
2020-09-29 11:28:23 -1000

    Lab 7

---
+++ Lab 7/ex4.js
@@ -0,0 +1,15 @@
+// -- Price depends on quantity
+var quantity =10;
+if( quantity > 0 && quantity < 10 ){
+    price = 100;
+}
+else if( quantity >= 10 && quantity < 25 ){
+    price = 50 ;
+}
+else if( quantity >= 25 ){
+     price = 35;
+}
+else{
+    price = "no purchase" ;
+}
+console.log( quantity + ' products will cost $' + price + ' each.' ) ;
