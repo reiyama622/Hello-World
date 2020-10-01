ead848ef4cfd9614eaa6dd07b9d5364d6d4a04e5
reiyama622 <--git>
2020-10-01 11:51:52 -1000

    Lab 8

--- Lab 8/ex1.js
+++ Lab 8/ex1.js
@@ -1,11 +1,6 @@
-var ageCount = 1;
+var count;
 var age = 21;
-while(ageCount < age){
-    if(ageCount >= age/2){
-        console.log("Im old!");
-        break;
-    }
-    console.log(`age ${ageCount}`);
-    ageCount++;
+for(count = 50; count > age; count--){
+    console.log(`age ${count}`);
 }
-console.log(`Rei is ${ageCount} years old`);
+console.log(`Rei is ${age} years old`);
