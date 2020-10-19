b0e14f92dbe5fc1f90fab333877967612ac32cee
reiyama622 <--git>
2020-10-18 14:39:43 -1000

    Lab 11

--- Lab 11/ex3.js
+++ Lab 11/ex3.js
@@ -4,5 +4,5 @@ var attributes = name + ";" + age + ";" + (age + 0.5) + ";" + (0.5 - age);
 var pieces = attributes.split(";");
 for(var part of pieces){
     console.log(`${part} is a ${typeof part}`);
-console.log(pieces.join(","));
+    console.log(pieces.join(","));
 }
